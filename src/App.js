a535a1657bc5c7fc123a175b44f91546abaf41d0
Bandana Singh <bandanasingh@Bandanas-MacBook-Pro.local>
2023-06-06 22:15:00 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGears } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section class="section-row">
+        <h1>Hi, I am Bandana</h1>
+        <h3>
+          Under construction  <FontAwesomeIcon icon={faGears} />
+        </h3>
+      </section>
+      <section class="section-row">
+      </section>
     </div>
   );
 }
